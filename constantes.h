b865cdfec73d0aef2814572a46434a68fa11cc7b
ANDRÉ FILIPE GOMES ROCHA <116861543+Andrefgr24@users.noreply.github.com>
2023-05-28 11:12:49 -0300

    Update constantes.h
    Comentários

--- constantes.h
+++ constantes.h
@@ -1,8 +1,11 @@
-#ifndef _CONSTANTES_H_
-#define _CONSTANTES_H_
+// Código para definições de constantes:
 
-#define MAX_ALUNO 100
+#ifndef _CONSTANTES_H_ // Verifica se a constante não foi definida anteriormente
+#define _CONSTANTES_H_ //  Definição da constante 
+
+// As três seguintes são definições de constantes que utilizaremos no código.
+#define MAX_ALUNO 100 
 #define MAX_PROFESSOR 100
 #define MAX_TURMA 100
 
-#endif
+#endif // Fim da condicional que não permite uma mesma constante ser adicionada 2 vezes.
