c2b6eb2732e72120790fa4f6f64f3cbd0b172845
ANDRÉ FILIPE GOMES ROCHA <116861543+Andrefgr24@users.noreply.github.com>
2023-05-28 20:51:44 -0300

    Update main.c
    Comentários

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
-#include "menus.h"
+// Importando códigos feitos anteriormente para rodarmos em nosso MAIN()
+#include "menus.h" 
 #include "tratadores.h"
 #include "dados.h"
 #include <stdio.h>
 #include "constantes.h"
 #include <stdlib.h>
-
+/*
+ -------Programa principal-------
+*/ 
 int main(int argc, char const *argv[])
-{
+// Definindo Variaveis
+{ 
     int op_menu_principal = 0;
     Aluno *alunos[MAX_ALUNO] = {NULL};
     Professor *professores[MAX_PROFESSOR] = {NULL};
@@ -16,12 +20,12 @@ int main(int argc, char const *argv[])
     int qtd_atual_professor = 0;
     int qtd_atual_turma = 0;
 
-    do
+    do //Loop do-while
     {
         op_menu_principal = menu_principal();
-        switch (op_menu_principal)
+        switch (op_menu_principal) // Caso op_menu_principal igual a 1,2,3,4 ou 5: 
         {
-        case 1:
+        case 1: 
             tratador_menu_aluno(alunos, &qtd_atual_aluno, turmas);
             break;
         case 2:
@@ -36,6 +40,7 @@ int main(int argc, char const *argv[])
         case 5:
             printf("Finalizando app...\n\n");
         }
-    } while (op_menu_principal != 5);
+    } while (op_menu_principal != 5); 
     return 0;
 }
+/* O programa continua a execução até que se escolha uma opção válida ou até decidir sair*/
